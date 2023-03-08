bdac9ca8e6b5fc22ec31b224c2e1245998f576fa
Minh Binh <>
2023-03-08 16:13:02 +0700

    test

--- pages/[urlRewriteName].jsx
+++ pages/[urlRewriteName].jsx
@@ -26,6 +26,7 @@ import NewsItemSmall from "../source/components/News/NewsItemSmall";
 import NewsItem from "../source/components/News/NewsItem";
 import AdsRow728 from "../source/components/Ads/AdsRow728";
 import AdsBox345 from "../source/components/Ads/AdsBox345";
+import LoadingOverlayContent from "../source/components/Loading/LoadingOverlayContent";
 
 export async function getServerSideProps(context) {
   const { query, res } = context;
@@ -169,23 +170,7 @@ const NewsDetail = (props) => {
                       <div className="jeg_posts jeg_load_more_flag">
                         {!isEmptyObject(listNewsRelate) ? listNewsRelate.map(newsItem => <NewsItem key={`key-${newsItem.CrawlerDataId}`} newsItem={newsItem} />) : ''}
                       </div>
-                      <div className="module-overlay">
-                        <div className="preloader_type preloader_dot">
-                          <div className="module-preloader jeg_preloader dot">
-                            <span></span><span></span><span></span>
-                          </div>
-                          <div className="module-preloader jeg_preloader circle">
-                            <div className="jnews_preloader_circle_outer">
-                              <div className="jnews_preloader_circle_inner"></div>
-                            </div>
-                          </div>
-                          <div className="module-preloader jeg_preloader square">
-                            <div className="jeg_square">
-                              <div className="jeg_square_inner"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                      <LoadingOverlayContent />
                     </div>
                     {/* <div className="jeg_block_navigation">
                       <div className="navigation_overlay">
@@ -211,28 +196,7 @@ const NewsDetail = (props) => {
                         <div className="jeg_posts jeg_load_more_flag">
                           {!isEmptyObject(listNewsRelate) ? listNewsRelate.map(newsItem => <NewsItemSmall key={`key-${newsItem.CrawlerDataId}`} newsItem={newsItem} />) : ''}
                         </div>
-                        <div className="module-overlay">
-                          <div className="preloader_type preloader_dot">
-                            <div className="module-preloader jeg_preloader dot">
-                              <span></span><span></span><span></span>
-                            </div>
-                            <div className="module-preloader jeg_preloader circle">
-                              <div className="jnews_preloader_circle_outer">
-                                <div className="jnews_preloader_circle_inner"></div>
-                              </div>
-                            </div>
-                            <div className="module-preloader jeg_preloader square">
-                              <div className="jeg_square">
-                                <div className="jeg_square_inner"></div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="jeg_block_navigation">
-                        <div className="navigation_overlay">
-                          <div className="module-preloader jeg_preloader"><span></span><span></span><span></span></div>
-                        </div>
+                        <LoadingOverlayContent />
                       </div>
                     </div>
                   </div>
